b92a1ccb7675feb7c233f2278c35771fde87ada2
Rana Ahtsham Ul Haq <rana1ahtsham@gmail.com>
2024-07-13 19:48:56 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './App.css';
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, onChange] = useState<Value>(new Date());
+
+    return (
+        <div className="App">
+            <Calendar onChange={onChange} value={value} />
+        </div>
+    );
 }
 
 export default App;
